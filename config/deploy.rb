443ccc4c6b9f38468b488312957ca9ffabc45c7c
Stuart Marquis <marquis@columbia.edu>
2017-04-25 13:27:39 -0400

    fix conflicts

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,5 @@
 # config valid only for current version of Capistrano
-<<<<<<< HEAD
-lock '3.7.1'
-=======
-# lock '3.7.2'
+lock '3.7.2'
 
 set :application, 'marc_liberation'
 set :repo_url, "https://github.com/pulibrary/marc_liberation.git"
