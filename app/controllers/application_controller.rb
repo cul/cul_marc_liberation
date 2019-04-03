3d92f9861c33d1b7d2eaad3611b0cb9bc751810b
Stuart Marquis <stuartmarquis@gmail.com>
2019-04-03 11:16:02 -0400

    True CAS SSO logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,19 @@ class ApplicationController < ActionController::Base
   #   request.referrer
   # end
 
+  def after_sign_out_path_for(_resource_or_scope)
+    cas_opts = YAML.load_file(File.join(Rails.root, 'config', 'cas.yml'))[Rails.env] || {}
+
+    # No CAS.
+    return root_path unless cas_opts['host'] && cas_opts['logout_url']
+
+    # Full CAS logout + application logout page looks like this:
+    # https://cas.columbia.edu/cas/logout?service=https://helpdesk.cul.columbia.edu/welcome/logout
+    cas_logout_url = 'https://' + cas_opts['host'] + cas_opts['logout_url']
+    return "#{cas_logout_url}?service=#{root_url}"
+  end
+
+
   # Columbia Authentication
   include Devise::Controllers::Helpers
   devise_group :user, contains: [:user]
