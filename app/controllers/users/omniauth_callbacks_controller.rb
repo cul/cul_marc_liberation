bbf419fe987024262a72b0ec1eb032f48c7dc3c5
Stuart Marquis <stuartmarquis@gmail.com>
2019-04-03 09:01:07 -0400

    Add Users::OmniauthCallbacksController#new_session_path

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,15 @@
 # Columbia Authentication
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-
   include Cul::Omniauth::Callbacks
 
-  def affiliations(user, affils)
-    user.affils = affils.sort
+  def new_session_path(_scope)
+    new_user_session_path # this accomodates Users namespace of the controller
   end
 
+  def affiliations(user, affils)
+    return unless user
+    user.affils = affils.sort
+  end
 end
 
 # 
